d1029cb25c3fc57c9bc2ed10cbcf7ad14bb5a101
김지은 <kimmjieun@gmail.com>
2021-10-14 17:58:50 +0900

    엔티티 설계시 주의점
    - Setter 사용 x (예제에서는 사용)
    - 지연 로딩 (LAZY)
    - 컬렉션 필드에서 초기화
    - 연관관계 메서드

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,19 +17,36 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member; // 주인
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
+    // order_date 로 컬럼명 바껴서 테이블에 저장
     private LocalDate orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus; // 주문상태 : [Order, Cancel]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
