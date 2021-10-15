7b082d3d68958e429eb73d0e8feca5e18ed2a23f
김지은 <kimmjieun@gmail.com>
2021-10-15 17:57:27 +0900

    session 5. 상품 도메인 개발 - 상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
 
+import jpabook.jpashop.exception.NotEnoughStockException;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +24,18 @@ public abstract class Item {
     private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<>();
+    private List<Category> categories = new ArrayList<Category>();
 
+    //==비즈니스 로직==//
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
