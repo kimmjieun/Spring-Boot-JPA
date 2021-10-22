8c261921ba40e854c69c43214c9c82709975591f
김지은 <kimmjieun@gmail.com>
2021-10-22 18:01:19 +0900

    next. QueryDSL 소개

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,19 +1,31 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
+import static jpabook.jpashop.domain.QMember.member;
+import static jpabook.jpashop.domain.QOrder.*;
+
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
     private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public OrderRepository(EntityManager em){
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
 
     public void save(Order order){
         em.persist(order);
@@ -29,6 +41,62 @@ public class OrderRepository {
                 .getResultList();
     }
 
+    public List<Order> findAll(OrderSearch orderSearch) {
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()),member.name.like(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName){
+        if(!StringUtils.hasText(memberName)){
+            return null;
+        }
+        else{
+            return member.name.like(memberName);
+        }
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond){
+        if(statusCond == null){
+            return null;
+        }
+        return order.status.eq(statusCond);
+    }
+
+
+//    /**
+//     * JPA Criteria
+//     */
+//    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+//        Root<Order> o = cq.from(Order.class);
+//        Join<Object, Object> m = o.join("member", JoinType.INNER);
+//
+//        List<Predicate> criteria = new ArrayList<>();
+//
+//        //주문 상태 검색
+//        if (orderSearch.getOrderStatus() != null) {
+//            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+//            criteria.add(status);
+//        }
+//        //회원 이름 검색
+//        if (StringUtils.hasText(orderSearch.getMemberName())) {
+//            Predicate name =
+//                    cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+//            criteria.add(name);
+//        }
+//
+//        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+//        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+//        return query.getResultList();
+//    }
+//
     public List<Order> findAllByString(OrderSearch orderSearch) {
         //language=JPAQL
         String jpql = "select o From Order o join o.member m";
